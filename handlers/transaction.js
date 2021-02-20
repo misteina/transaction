0f00209138ee7e4cc765b6f2cf2637a3d664cc01
Enyinna <enyinna.tech@gmail.com>
2021-02-20 22:06:17 +0100

    Transaction handler

--- handlers/transaction.js
+++ handlers/transaction.js
@@ -1,3 +1,71 @@
 module.exports = function (req, res){
     
+    const amount = req.body.amount;
+    const currency = req.body.currency;
+    const source = req.cookies.id;
+    const target = req.body.target;
+
+    const errors = [];
+
+    if (isNaN(userId)){
+        errors.push("Invalid user id");
+    }
+    if (isNaN(amount)){
+        errors.push("Invalid amount selected");
+    }
+    if (currency !== 'Bitcoin' && currency !== 'Ethereum'){
+        errors.push("Invalid currency selected");
+    }
+    if (isNaN(source)){
+        errors.push("Invalid sender");
+    }
+    if (isNaN(target)) {
+        errors.push("Invalid receiver");
+    }
+
+    if (errors.length === 0){
+
+        const connection = require('../lib/connection');
+
+        connection.query(
+            "SELECT MaxAmount FROM Users WHERE id = ?",
+            [userId],
+            function (error, results, fields){
+                if (!error){
+                    if (results.length === 1){
+                        
+                        if (amount <= results[0].MaxAmount){
+                            connection.query(
+                                "INSERT INTO Transactions (Amount, Currency, Source, Target) VALUES (?, ?, ?, ?)",
+                                [amount, currency, source, target],
+                                function (error, results, fields) {
+                                    if (!error && !isNaN(results.insertId)) {
+
+                                        const addToQueue = require('../queue/job');
+
+                                        let params = [connection, results.insertId, amount, currency, source, target];
+
+                                        addToQueue(params);
+
+                                        res.json({success: `Transaction successfully submitted with ID ${results.insertId}`});
+
+                                    } else {
+                                        res.status(406).json({ error: error });
+                                    }
+                                }
+                            );
+                        } else {
+                            res.status(406).json({ error: "Maximum amount per transaction exceeded" });
+                        }
+                    } else {
+                        res.status(406).json({ error: error });
+                    }
+                } else {
+                    res.status(406).json({ error: "Request failed" });
+                }
+            }
+        );
+    } else {
+        res.status(406).json({ error: errors });
+    }
 }
