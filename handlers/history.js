ff137e1040567087e46fcdc99253d92c0e1581b4
Enyinna <enyinna.tech@gmail.com>
2021-02-21 13:22:17 +0100

    Transaction history

--- handlers/history.js
+++ handlers/history.js
@@ -9,11 +9,28 @@ module.exports = function (req, res) {
         const connection = require('../lib/connection');
 
         connection.query(
-            "SELECT * FROM Transaction WHERE Source = ? ORDER BY Created ASC LIMIT 10",
-            [userId],
+            "SELECT * FROM Transaction WHERE Source = ? OR Target = ? ORDER BY Created ASC LIMIT 10",
+            [userId, userId],
             function (error, results, fields){
                 if (!error && results.length > 0) {
-                    res.json({ data: results });
+
+                    let rows = [];
+
+                    for (let row of results){
+                        let flow = (userId === row.Source)? 
+                            ["Sent", "To", row.Target] : 
+                            ["Received", "From", row.Source];
+
+                        rows.push(
+                            {
+                                [flow[0]]: row.Amount,
+                                currency: row.Currency,
+                                [flow[1]]: [flow[2]],
+                                processed: row.Processed
+                            }
+                        )
+                    }
+                    res.json({ data: rows });
                 } else {
                     res.status(406).json({ error: "Request failed" });
                 }
